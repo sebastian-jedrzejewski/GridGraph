6a2b6a02e6a324104ca9022d8b204a42a25acfd8
Sebastian Jędrzejewski <sebastian.jedrzejewski12@gmail.com>
2022-03-27 23:47:28 +0200

    implementation of Dijkstra's algorithm - version 1

---
+++ GridGraph/Dijkstra.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#include "Dijkstra.h"
+
+d_result result_init(int n, int vertex_a, int *visited)
+{
+    d_result result = malloc(sizeof *result);
+    result->d = malloc(n * sizeof *(result->d));
+    result->p = malloc(n * sizeof *(result->p));
+
+    int i;
+    for(i = 0; i < n; i++)
+    {
+        *visited = 0;
+        if(i == vertex_a)
+            result->d[i] = 0;
+        else
+            result->d[i] = INT_MAX;
+        result->p[i] = -1;
+        visited++;
+    }
+    return result;
+}
+
+void result_free(d_result result)
+{
+    free(result->d);
+    free(result->p);
+    free(result);
+}
+
+d_result Dijkstra(graph graph, int vertex_a, int vertex_b)
+{
+    int n = graph->height * graph->width;
+    int *visited = malloc(n * sizeof *visited);
+
+    int i, j, x;
+    edge_list *iter = NULL;
+
+    d_result result = result_init(n, vertex_a, visited);
+
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; visited[j]; j++)
+            ;
+        
+        x = j;
+        for(j = x; j < n; j++)
+            if(result->d[j] < result->d[x] && !visited[j])
+             x = j;
+        
+        visited[x] = 1;
+
+        
+        iter = graph->list[x];
+        while(iter != NULL)
+        {
+            if(visited[iter->vertex])
+            {
+                iter = iter->next;
+                continue;
+            }
+            else
+                if(result->d[iter->vertex] > result->d[x] + iter->weight)
+                    result->d[iter->vertex] = result->d[x] + iter->weight;
+            result->p[iter->vertex] = x;
+            iter = iter->next;
+        }
+    }
+    return result;
+}
--- GridGraph/read.c
+++ GridGraph/read.c
@@ -8,6 +8,26 @@
 #define MAX_VERTICES_IN_LINE 128
 
 
+int path_fill(d_result result, int *nv, int from, int to)
+{
+    int l = 0;
+    int i;
+    for(i = to; i > -1; i = result->p[i])
+        nv[l++] = i;
+    return l;
+}
+
+void path_display(d_result result, int *nv, int l)
+{
+    int j;
+    for(j = l-1; j >= 0; j--)
+    {
+        if(j != 0)
+            printf("%d - ", nv[j]);
+        else
+            printf("%d ", nv[j]);
+    }
+}
 
 // Reads a graph from a file and saves it to the structure
 graph read_graph(FILE *f)
@@ -48,6 +68,34 @@ int read(FILE *file, int connectivity, int vertex_a, int vertex_b)
     graph g = read_graph(file);
     if(g == NULL)
         return 1;
-    else
+    else 
+    {
+        int i, j, l;
+        d_result result = Dijkstra(g, vertex_a, vertex_b);
+
+        int *nv = malloc(g->height * g->width * sizeof *nv);
+
+        if(vertex_b >= 0)
+        {
+            printf("Shortest path from %d to %d and its weight: ", vertex_a, vertex_b);
+            l = path_fill(result, nv, vertex_a, vertex_b);
+            path_display(result, nv, l);
+            printf("(%g)\n", result->d[vertex_b]);
+        }
+        else
+        {
+            printf("Shortest paths from %d to all vertices and their weights:\n\n", vertex_a);
+            for(i = 0; i < g->height * g->width; i++)
+            {
+                l = path_fill(result, nv, vertex_a, i);
+                printf("Path to %d (weight): ", i);
+                path_display(result, nv, l);
+                printf("(%g)\n", result->d[i]);
+            }
+        }
+
+        result_free(result);
+        free(nv);
         return 0;
+    }
 }

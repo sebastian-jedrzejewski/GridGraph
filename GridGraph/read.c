20356e3084f8841a9ab996a9eff6431907f1da0b
Sebastian Jędrzejewski <sebastian.jedrzejewski12@gmail.com>
2022-03-28 18:37:45 +0200

    bfs algorithm attached to the whole program

--- GridGraph/read.c
+++ GridGraph/read.c
@@ -3,6 +3,7 @@
 
 #include "graph.h"
 #include "read.h"
+#include "bfs.h"
 
 #define MAX_LINE_LEN 1024
 #define MAX_VERTICES_IN_LINE 128
@@ -30,6 +31,58 @@ void path_display(d_result result, int *nv, int l)
         else
             printf("%d ", nv[j]);
     }
+}
+
+void bfs_init(graph g, int vertex_a)
+{
+    int *visited = bfs(g, vertex_a > 0 ? vertex_a : 0);
+    int c = 0;
+    while(visited[c++] && c < g->height * g->width)
+        ;
+    if(c == g->height * g->width)
+        printf("Connectivity: connected\n\n");
+    else
+        printf("Connectivity: disconnected\n\n");
+}
+
+void dijkstra_init(graph g, int vertex_a, int vertex_b)
+{
+    int i, j, l;
+    d_result result = dijkstra(g, vertex_a, vertex_b);
+
+    int *nv = malloc(g->height * g->width * sizeof *nv);
+
+    if(vertex_b >= 0)
+    {
+        printf("Shortest path from %d to %d and its weight: ", vertex_a, vertex_b);
+        l = path_fill(result, nv, vertex_a, vertex_b);
+        if(l > 0)
+        {
+            path_display(result, nv, l);
+            printf("(%g)\n", result->d[vertex_b]);
+        }
+        else
+            printf("Path does not exist\n");
+    }
+    else
+    {
+        printf("Shortest paths from %d to all vertices and their weights:\n\n", vertex_a);
+        for(i = 0; i < g->height * g->width; i++)
+        {
+            l = path_fill(result, nv, vertex_a, i);
+            printf("Path to %d (weight): ", i);
+            if(l > 0)
+            {
+                path_display(result, nv, l);
+                printf("(%g)\n", result->d[i]);
+            }
+            else
+                printf("Path does not exist\n");
+        }
+    }
+
+    result_free(result);
+    free(nv);
 }
 
 // Reads a graph from a file and saves it to the structure
@@ -73,42 +126,16 @@ int read(FILE *file, int connectivity, int vertex_a, int vertex_b)
         return 1;
     else 
     {
-        int i, j, l;
-        d_result result = Dijkstra(g, vertex_a, vertex_b);
-
-        int *nv = malloc(g->height * g->width * sizeof *nv);
-
-        if(vertex_b >= 0)
+        if(connectivity == 1)
         {
-            printf("Shortest path from %d to %d and its weight: ", vertex_a, vertex_b);
-            l = path_fill(result, nv, vertex_a, vertex_b);
-            if(l > 0)
-            {
-                path_display(result, nv, l);
-                printf("(%g)\n", result->d[vertex_b]);
-            }
-            else
-                printf("Path does not exist\n");
-        }
-        else
-        {
-            printf("Shortest paths from %d to all vertices and their weights:\n\n", vertex_a);
-            for(i = 0; i < g->height * g->width; i++)
-            {
-                l = path_fill(result, nv, vertex_a, i);
-                printf("Path to %d (weight): ", i);
-                if(l > 0)
-                {
-                    path_display(result, nv, l);
-                    printf("(%g)\n", result->d[i]);
-                }
-                else
-                    printf("Path does not exist\n");
-            }
+            bfs_init(g, vertex_a);
         }
 
-        result_free(result);
-        free(nv);
+        if(vertex_a >= 0)
+        {
+            dijkstra_init(g, vertex_a, vertex_b);
+        }
+
         return 0;
     }
 }
--- GridGraph/read.h
+++ GridGraph/read.h
@@ -9,5 +9,7 @@ int read(FILE *file, int connectivity, int vertex_a, int vertex_b);
 graph read_graph(FILE *f);
 int path_fill(d_result result, int *nv, int from, int to);
 void path_display(d_result result, int *nv, int l);
+void bfs_init(graph g, int vertex_a);
+void dijkstra_init(graph g, int vertex_a, int vertex_b);
 
 #endif

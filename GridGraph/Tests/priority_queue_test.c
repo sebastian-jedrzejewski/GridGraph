8b58219d97e492800f6f7dbc7ff45e5f9215ed9a
Sebastian Jędrzejewski <sebastian.jedrzejewski12@gmail.com>
2022-04-10 19:40:08 +0200

    priority queue test finished

--- GridGraph/Tests/priority_queue_test.c
+++ GridGraph/Tests/priority_queue_test.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 
-#include "priority_queue.h"
+#include "../priority_queue.h"
 
-/*int main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     int n = argc > 1 ? atoi(argv[1]) : 10;
     FILE *f = argc > 2 ? fopen(argv[2], "w") : stdout;
     double *d = malloc(n * sizeof *d);
+    double *s = malloc(n * sizeof *s);
     pq p_queue = pq_init(n);
     
     srand(time(NULL));
@@ -26,14 +27,25 @@
             return EXIT_FAILURE;
     }
 
+    i = 0;
     fprintf(f, "Heap sort:\n");
     while(!pq_is_empty(p_queue))
     {
-        fprintf(f, "%g\n", d[pq_pop(p_queue, d)]);
+        fprintf(f, "%g\n", s[i] = d[pq_pop(p_queue, d)]);
+        if(i > 0)
+        {
+            if(s[i] < s[i-1])
+            {
+                fprintf(f, "Priority queue does not work properly. %g is not less than %g\n", s[i-1], s[i]);
+                return EXIT_FAILURE;
+            }
+        }
+        i++;
     }
 
     pq_free(p_queue);
 
+    fprintf(f, "Priority queue works properly.\n");
     return EXIT_SUCCESS;
 
-}*/
+}
